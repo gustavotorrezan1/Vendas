b36ce87a3d907f8407f2b9e51585752d41818719
gustavotorrezan1 <74027638+gustavotorrezan1@users.noreply.github.com>
2021-08-03 21:34:38 -0300

    Montada as classes
    .

--- src/util/Compra.java
+++ src/util/Compra.java
@@ -1,5 +1,4 @@
-package javaapplication1;
-
+package util;
 import java.util.Date;
 import java.util.List;
 import java.util.ArrayList;
@@ -13,6 +12,54 @@ public class Compra {
     private Date data;   //LocalDateTime
     private Date dataEntrega;
     private Date dataFinal;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+	public Date getDataEntrega() {
+		return dataEntrega;
+	}
+	public void setDataEntrega(Date dataEntrega) {
+		this.dataEntrega = dataEntrega;
+	}
+	public Date getDataFinal() {
+		return dataFinal;
+	}
+	public void setDataFinal(Date dataFinal) {
+		this.dataFinal = dataFinal;
+	}
+    
+    
 }
-          
-
